af6390fbf4c294f3dbb6bec312499faf3fba9411
santo <superium@126.com>
2025-05-02 19:01:10 +0800

    update syntax-cn

--- src/expression/eval.rs
+++ src/expression/eval.rs
@@ -432,6 +432,9 @@ impl Expression {
                     let r = rhs.eval_mut(false, env, depth + 1)?; //TODO: allow dynamic Key? x.log log=builtin@log
 
                     return match (l, r) {
+                        (Expression::List(m), Expression::Integer(n)) => {
+                            Self::index_slm(Expression::List(m), Expression::Integer(n))
+                        }
                         (Expression::Map(m), n) => Self::index_slm(Expression::Map(m), n),
                         (Self::Symbol(m), Self::Symbol(n)) => {
                             Ok(Self::String(format!("{}.{}", m, n)))
